0db905cc3c0c23dee3cffe8afa65904dfaeb3643
harshitrajsinha <raj.harshitsinha08@gmail.com>
2026-09-23 12:42:57 +0530

    bugfix: adding missing seed user

--- scripts/seed_users.py
+++ scripts/seed_users.py
@@ -12,6 +12,31 @@
 # )
 
 
+# import bcrypt
+
+# password = "my-secret-password"
+
+# hashed_password = bcrypt.hashpw(
+#     password.encode("utf-8"),
+#     bcrypt.gensalt()
+# )
+
+# print(hashed_password)
+
+
+
+# password = "my-secret-password"
+# stored_hash = hashed_password
+
+# if bcrypt.checkpw(
+#     password.encode("utf-8"),
+#     stored_hash
+# ):
+#     print("Password is correct")
+# else:
+#     print("Invalid password")
+
+
 # def get_seed_password() -> str:
 #     password = os.getenv("SEED_USER_PASSWORD")
 #     if not password:
@@ -53,7 +78,7 @@ def seed_users(session) -> None:
     users = [
         User(
             email="admin@eliciusenergy.com",
-            password_hash="$2b$12$zx7oZdO2jYQmhJK6Gu4FI.h5qeP9MtPicqixNb1gGjVt7Cv.amsZe",
+            password_hash="$2b$12$UPc9qrjOGzn4.UOK9hBzFO6XW8YHdenop2x2nR3A8TujZO8mj9.6S",
             is_active=True,
             role=UserRole.ADMIN,
         ),
@@ -62,6 +87,12 @@ def seed_users(session) -> None:
             password_hash="$2b$12$jGfJqfTrkgsxaF.KEyok9OW8GPW1eojoTSzsILnVYkGvfgOC0tDpq",
             is_active=True,
             role=UserRole.ADMIN,
+        ),
+        User(
+            email="newuser@gmail.com",
+            password_hash="$2b$12$pNRxjWndhlMdrqMvbWSxeOunvIdg46ZOvk.nS79mAIopiBSdVyy4G",
+            is_active=True,
+            role=UserRole.USER,
         ),
     ]
 
